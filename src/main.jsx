0d3d85dd8fffd872103f5411a66dd59cc3590dae
kumarapubudu116-cmyk <kumarapubudu116@gmail.com>
2026-08-07 22:57:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from import App from "./Khancraftz.jsx";
+import App from "./Khancraftz.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
